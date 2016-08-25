c8cd51934de2118c4a9f78d5542996b0499c8e1a
LizRuelas <liz.ruelas.borda@gmail.com>
2016-08-25 14:11:16 -0700

    subiendo archivos

---
+++ app.js
@@ -0,0 +1,10 @@
+var numero;  
+       
+numero=prompt("Ingrese el numero por favor : ");
+
+if (parseInt(numero)>0) {
+	window.alert("El numero es  POSITIVO");
+} else {
+	window.alert("El numero es NEGATIVO");
+}
+
